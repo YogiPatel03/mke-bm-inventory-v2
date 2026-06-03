43d2eacb9126fc9fb0a12a090cd0deef4110a736
YogiPatel03 <yogipatel2003@gmail.com>
2026-06-03 13:22:18 -0500

    add room service

---
+++ src/main/java/com/yogipatel/inventory/room/RoomService.java
@@ -0,0 +1,23 @@
+package com.yogipatel.inventory.room;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomService {
+    private final RoomRepository roomRepository;
+
+    public RoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    public List<Room> getAllRooms() {
+        return roomRepository.findAllByOrderByNameAsc();
+    }
+
+    public Room createRoom(String name, String description) {
+        Room room = new Room(name, description);
+        return roomRepository.save(room);
+    }
+}
